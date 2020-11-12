b5d85eaf5070ecb5c8b0cd3e96143a6dcf379674
Calvin <63000172+calvintettey@users.noreply.github.com>
2020-11-12 09:56:22 +0000

    building Contacts Screen

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import LoginScreen from './LoginScreen';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import ContactsScreen from "./ContactsScreen";
+import Contact from "./components/Contact";
+import LoginScreen from "./LoginScreen";
 
 export default function App() {
   return (
-    <View>
-     <LoginScreen/>
+    <View style={styles.container}>
+      {/* <LoginScreen/> */}
+
+      <ContactsScreen />
+
     </View>
   );
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    marginTop:100
+    // flex: 1,
+    // backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+});
