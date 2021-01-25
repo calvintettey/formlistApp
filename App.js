64af0652ef44c8b83a1a09c604dd059e5d81e32a
Calvin <63000172+calvintettey@users.noreply.github.com>
2021-01-25 23:19:30 +0000

    added navigation folders&files moved main code to mavigation.js

--- App.js
+++ App.js
@@ -1,25 +1,19 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, View } from "react-native";
-import ContactsScreen from "./ContactsScreen";
-import Contact from "./components/Contact";
-import LoginScreen from "./LoginScreen";
+import { Provider } from "react-redux";
+import store from "./src/redux/store";
+import AppContainer from "./src/navigation/navigation";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-    
-      {/* <LoginScreen/> */}
-
-      <ContactsScreen />
-
-    </View>
+    <Provider store={store}>
+      <AppContainer />
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    marginTop:100
+    marginTop: 100,
     // flex: 1,
     // backgroundColor: '#fff',
     // alignItems: 'center',
---
+++ src/navigation/navigation.js
@@ -0,0 +1,30 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import ContactsScreen from "./ContactsScreen";
+import Contact from "./components/Contact";
+import LoginScreen from "./LoginScreen";
+import { Provider } from "react-redux";
+import store from './src/redux/store'
+
+export default function AppContainer() {
+  return (
+    <Provider store={store}>
+      <View style={styles.container}>
+        {/* <LoginScreen/> */}
+
+        <ContactsScreen />
+      </View>
+    </Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 100,
+    // flex: 1,
+    // backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+});
