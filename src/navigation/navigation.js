fa9d0bcd179c1be034d97f78beebc9e244a1c043
Calvin <63000172+calvintettey@users.noreply.github.com>
2021-01-26 18:54:04 +0000

    installed react navigation dependencies

--- src/navigation/navigation.js
+++ src/navigation/navigation.js
@@ -1,21 +1,32 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, View } from "react-native";
+import { NavigationContainer, StackActions } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import ContactsScreen from "./ContactsScreen";
 import Contact from "./components/Contact";
 import LoginScreen from "./LoginScreen";
 import { Provider } from "react-redux";
 import store from './src/redux/store'
+import SignupScreen from "../../SignupScreen";
 
+const Stack = createStackNavigator();
 export default function AppContainer() {
   return (
-    <Provider store={store}>
-      <View style={styles.container}>
-        {/* <LoginScreen/> */}
+      <NavigationContainer>
+          <Stack.Navigator>
+              <Stack.Screen name="Log In" component={LoginScreen} />
+              <Stack.Screen name="Register" component={SignupScreen} />
+              <Stack.Screen name="Contacts" component={ContactsScreen} />
+          </Stack.Navigator>
+      </NavigationContainer>
+    // <Provider store={store}>
+    //   <View style={styles.container}>
+    //     {/* <LoginScreen/> */}
 
-        <ContactsScreen />
-      </View>
-    </Provider> 
+    //     <ContactsScreen />
+    //   </View>
+    // </Provider> 
   );
 }
 
