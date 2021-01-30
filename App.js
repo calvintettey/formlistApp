f7d33fac23f6e4b8b07e9f0a954c73d18f5f9331
Calvin <63000172+calvintettey@users.noreply.github.com>
2021-01-30 00:56:44 +0000

    minor fixes

--- App.js
+++ App.js
@@ -13,13 +13,3 @@ export default function App() {
     </Provider>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     marginTop: 100,
-//     // flex: 1,
-//     // backgroundColor: '#fff',
-//     // alignItems: 'center',
-//     // justifyContent: 'center',
-//   },
-// });
